25ed9b156021d9b45adbb13fea1000e3651f4a49
Ivan Sotnikov <ivan@coldhiber.com>
2021-02-12 22:11:56 +0100

    Added draft solution for Longest Palindromic Substring challenge

---
+++ longest_palindromic_substring/longest_palindromic_substring.go
@@ -0,0 +1,38 @@
+package main
+
+func longestPFromCenter(s string, left, right int) int {
+	l := left
+	r := right
+
+	for l >= 0 && r < len(s) && s[l] == s[r] {
+		l++
+		r++
+	}
+
+	return r - l - 1
+}
+
+func longestPalindrome(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+
+	start := 0
+	end := 0
+	for i := 0; i < len(s); i++ {
+		l1 := longestPFromCenter(s, i, i)
+		l2 := longestPFromCenter(s, i, i+1)
+
+		l := l1
+		if l2 > l1 {
+			l = l2
+		}
+
+		if l > (end - start) {
+			start = i - (l-1)/2
+			end = i + l/2
+		}
+	}
+
+	return s[start : end+3]
+}
---
+++ longest_palindromic_substring/longest_palindromic_substring_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func assertStringResult(t *testing.T, s, exp string) {
+	a := longestPalindrome(s)
+	if a != exp {
+		t.Errorf("Expected: %v Actual: %v", exp, a)
+	}
+}
+
+func Test_longestPalindrome(t *testing.T) {
+	assertStringResult(t, "babdrrggbbbabbb", "bab")
+}
