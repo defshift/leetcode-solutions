b754b758a5bf3ba8e8a99dbef806078fb81773d8
Ivan Sotnikov <ivan@coldhiber.com>
2021-02-20 19:12:04 +0100

    Optimized solution for Container With Most Water challenge

--- container_with_most_water/container_with_most_water.go
+++ container_with_most_water/container_with_most_water.go
@@ -1,18 +1,26 @@
 package main
 
 func maxArea(height []int) int {
-	maxArea := 0
-	for i := 0; i < len(height)-1; i++ {
-		for j := i + 1; j < len(height); j++ {
-			min := height[i]
-			if height[j] < min {
-				min = height[j]
-			}
+	i := 0
+	j := len(height) - 1
 
-			area := min * (j - i)
-			if area > maxArea {
-				maxArea = area
-			}
+	maxArea := 0
+	for i != j {
+		min := height[i]
+		if height[j] < min {
+			min = height[j]
+		}
+
+		area := (j - i) * min
+
+		if area > maxArea {
+			maxArea = area
+		}
+
+		if height[j] > height[i] {
+			i++
+		} else {
+			j--
 		}
 	}
 
