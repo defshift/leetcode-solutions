877d7ac4686f8e6c2c7a2c6f5598587a218bb880
Ivan Sotnikov <ivan@coldhiber.com>
2021-03-05 21:58:01 +0100

    Cleaned solution for Integer to roman challenge

--- integer_to_roman/integer_to_roman.go
+++ integer_to_roman/integer_to_roman.go
@@ -5,21 +5,19 @@ import (
 )
 
 const (
-	I   = 1
-	II  = 2
-	III = 3
-	IV  = 4
-	V   = 5
-	IX  = 9
-	X   = 10
-	XL  = 40
-	L   = 50
-	XC  = 90
-	C   = 100
-	CD  = 400
-	D   = 500
-	CM  = 900
-	M   = 1000
+	I  = 1
+	IV = 4
+	V  = 5
+	IX = 9
+	X  = 10
+	XL = 40
+	L  = 50
+	XC = 90
+	C  = 100
+	CD = 400
+	D  = 500
+	CM = 900
+	M  = 1000
 )
 
 type pair struct {
@@ -29,8 +27,6 @@ type pair struct {
 
 var mapping = []pair{
 	{I, "I"},
-	{II, "II"},
-	{III, "III"},
 	{IV, "IV"},
 	{V, "V"},
 	{IX, "IX"},
