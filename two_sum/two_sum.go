ab275c4e292ac4e4d0bc2b40bfb893233991b933
Ivan Sotnikov <ivan@coldhiber.com>
2023-02-03 18:40:57 +0100

    feat: add two sum solutions in go

--- two_sum/two_sum.go
+++ two_sum/two_sum.go
@@ -1,5 +1,38 @@
 package main
 
+func twoSum2rounds(nums []int, target int) []int {
+	valueMap := map[int]int{}
+
+	for i, v := range nums {
+		valueMap[v] = i
+	}
+
+
+	for i, v := range nums {
+		complement := target - v
+	
+		if c, ok := valueMap[complement]; ok && i != c {
+			return []int{i, c}
+		}
+	}
+
+	return []int{0, 0} 
+}
+
+func twoSum1Round(nums []int, target int) []int {
+	valueMap := map[int]int{}
+
+	for i, v := range nums {
+		if index, ok := valueMap[v]; ok {
+			return []int{i, index}
+		} else {
+			valueMap[target - v] = i
+		}
+	}
+
+	return []int{0, 0} 
+}
+
 func twoSum(nums []int, target int) []int {
 	for first := 0; first < len(nums)-1; first++ {
 		for second := first + 1; second < len(nums); second++ {
